2b681f1f36f7e1f5b6092288098de49b19961015
azertyfun <azertyfun_poubelle@hotmail.fr>
2017-08-06 21:49:50 +0200

    Fixed typo in boot_test.go

--- boot_test.go
+++ boot_test.go
@@ -65,7 +65,7 @@ func TestApps(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if r.StatusCode != 200 && t.StatusCode != 500 {
+	if r.StatusCode != 200 && r.StatusCode != 500 {
 		t.Fatalf("expected 200 or 500 (depending on wether the database has been built or not), got %v", r.StatusCode)
 	}
 }
