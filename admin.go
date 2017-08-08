44c5064132256ed41c61506b9cba484e338d82a1
azertyfun <azertyfun_poubelle@hotmail.fr>
2017-08-08 12:10:33 +0200

    Implemented missing tags for getApp()

--- admin.go
+++ admin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -92,18 +93,25 @@ func AdminRebuildDBHandler(ctx *handlerContext, w http.ResponseWriter, r *http.R
 		log.Fatal(err)
 	}
 
+	// tag_ids and screenshot_urls are Marshaled arrays, hence the BLOB type.
 	sqlStmt := `
 			create table apps (
 				id text not null primary key,
 				name text,
 				author_id integer,
-				category text,
+				tag_ids blob,
 				description text,
 				published_date integer,
 				pbw_url text,
 				rebble_ready integer,
 				updated integer,
-				version text
+				version text,
+				support_url text,
+				author_url text,
+				source_url text,
+				screenshot_urls blob,
+				banner_url text,
+				icon_url text
 			);
 			delete from apps;
 		`
@@ -125,24 +133,52 @@ func AdminRebuildDBHandler(ctx *handlerContext, w http.ResponseWriter, r *http.R
 		return 500, fmt.Errorf("%q: %s", err, sqlStmt)
 	}
 
+	// Placeholder until we implement an actual collections system.
+	sqlStmt = `
+			create table categories (
+				id text not null primary key,
+				name text,
+				color text
+			);
+			delete from categories;
+		`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		return 500, fmt.Errorf("%q: %s", err, sqlStmt)
+	}
+
 	tx, err := db.Begin()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	stmt, err := tx.Prepare("INSERT INTO apps(id, name, author_id, category, description, published_date, pbw_url, rebble_ready, updated, version) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	stmt, err := tx.Prepare("INSERT INTO apps(id, name, author_id, tag_ids, description, published_date, pbw_url, rebble_ready, updated, version, support_url, author_url, source_url, screenshot_urls, banner_url, icon_url) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
 
 	authors := make(map[string]int)
-	lastId := 0
+	categoriesNames := make(map[string]string)
+	categoriesColors := make(map[string]string)
+	lastAuthorId := 0
 	path, errc := walkFiles("PebbleAppStore/apps")
 	for item := range path {
 		//fmt.Fprintf(w, "File: %s<br />", item)
-		app := parseApp(item, &authors, &lastId)
-		_, err = stmt.Exec(app.Id, app.Name, app.Author.Id, app.Category, app.Description, app.Published.UnixNano(), app.AppInfo.PbwUrl, app.AppInfo.RebbleReady, app.AppInfo.Updated.UnixNano(), app.AppInfo.Version)
+		app := parseApp(item, &authors, &lastAuthorId, &categoriesNames, &categoriesColors)
+
+		tag_ids_s := make([]string, 1)
+		tag_ids_s[0] = app.AppInfo.Tags[0].Id
+		tag_ids, err := json.Marshal(tag_ids_s)
+		if err != nil {
+			log.Fatal("Could not marshal app tags:", err)
+		}
+		screenshot_urls, err := json.Marshal(app.Assets.Screenshots)
+		if err != nil {
+			log.Fatal("Could not marshal app screenshots:", err)
+		}
+
+		_, err = stmt.Exec(app.Id, app.Name, app.Author.Id, tag_ids, app.Description, app.Published.UnixNano(), app.AppInfo.PbwUrl, app.AppInfo.RebbleReady, app.AppInfo.Updated.UnixNano(), app.AppInfo.Version, app.AppInfo.SupportUrl, app.AppInfo.AuthorUrl, app.AppInfo.SourceUrl, screenshot_urls, app.Assets.Banner, app.Assets.Icon)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -153,6 +189,13 @@ func AdminRebuildDBHandler(ctx *handlerContext, w http.ResponseWriter, r *http.R
 
 	for author, id := range authors {
 		_, err = tx.Exec("INSERT INTO authors(id, name) VALUES(?, ?)", id, author)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	for id, category := range categoriesNames {
+		_, err = tx.Exec("INSERT INTO categories(id, name, color) VALUES(?, ?, ?)", id, category, categoriesColors[id])
 		if err != nil {
 			log.Fatal(err)
 		}
--- application.go
+++ application.go
@@ -23,17 +23,21 @@ type RebbleApplication struct {
 	Id          string        `json:"id"`
 	Name        string        `json:"title"`
 	Author      RebbleAuthor  `json:"author"`
-	Category    string        `json:"category_name"`
 	Description string        `json:"description"`
 	Published   JSONTime      `json:"published_date"`
 	AppInfo     RebbleAppInfo `json:"appInfo"`
+	Assets      RebbleAssets  `json:"assets"`
 }
 
 type RebbleAppInfo struct {
-	PbwUrl      string   `json:"pbwUrl"`
-	RebbleReady bool     `json:"rebbleReady"`
-	Updated     JSONTime `json:"updated"`
-	Version     string   `json:"version"`
+	PbwUrl      string           `json:"pbwUrl"`
+	RebbleReady bool             `json:"rebbleReady"`
+	Tags        []RebbleCategory `json:"tags"`
+	Updated     JSONTime         `json:"updated"`
+	Version     string           `json:"version"`
+	SupportUrl  string           `json:"supportUrl"`
+	AuthorUrl   string           `json:"authorUrl"`
+	SourceUrl   string           `json:"sourceUrl"`
 }
 
 type RebbleAuthor struct {
@@ -41,15 +45,33 @@ type RebbleAuthor struct {
 	Name string `json:"name"`
 }
 
+type RebbleAssets struct {
+	Banner      string   `json:"appBanner"`
+	Icon        string   `json:"appIcon"`
+	Screenshots []string `json:"screenshots"`
+}
+
+type RebbleCategory struct {
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Color string `json:"color"`
+}
+
 // PebbleApplication is used by the parseApp() function. It matches directly the `{id}.json` format.
 type PebbleApplication struct {
-	Id          string                   `json:"id"`
-	Name        string                   `json:"title"`
-	Author      string                   `json:"author"`
-	Category    string                   `json:"category_name"`
-	Description string                   `json:"description"`
-	Published   JSONTime                 `json:"published_date"`
-	Release     PebbleApplicationRelease `json:"latest_release"`
+	Id            string                   `json:"id"`
+	Name          string                   `json:"title"`
+	Author        string                   `json:"author"`
+	CategoryId    string                   `json:"category_id"`
+	CategoryName  string                   `json:"category_name"`
+	CategoryColor string                   `json:"category_color"`
+	Description   string                   `json:"description"`
+	Published     JSONTime                 `json:"published_date"`
+	Release       PebbleApplicationRelease `json:"latest_release"`
+	Website       string                   `json:"website"`
+	Source        string                   `json:"source"`
+	Screenshots   PebbleScreenshotImages   `json:"screenshot_images"`
+	HeaderImages  PebbleHeaderImages       `json:"header_images"`
 }
 
 type PebbleApplicationRelease struct {
@@ -59,7 +81,39 @@ type PebbleApplicationRelease struct {
 	Version   string   `json:"version"`
 }
 
-func parseApp(path string, authors *map[string]int, lastId *int) *RebbleApplication {
+/*
+ * Screenshots and Header images are stored as arrays in the json files. But when there is no screenshot or header, the array is just an empty string, which is fine with dynamically typed languages. However, with Go, we have to redefine UnmarshalJSON for those types.
+ */
+type PebbleHeaderImages []PebbleHeaderImage
+type PebbleScreenshotImages []PebbleScreenshotImage
+
+type PebbleHeaderImage struct {
+	Orig string `json:"orig"`
+}
+
+type PebbleScreenshotImage struct {
+	Screenshot string `json:"144x168"`
+}
+
+func (phi *PebbleHeaderImages) UnmarshalJSON(b []byte) error {
+	if len(b) == 0 || b[0] == '"' {
+		*phi = make([]PebbleHeaderImage, 0)
+		return nil
+	}
+
+	return json.Unmarshal(b, (*([]PebbleHeaderImage))(phi))
+}
+
+func (psi *PebbleScreenshotImages) UnmarshalJSON(b []byte) error {
+	if len(b) == 0 || b[0] == '"' {
+		*psi = make([]PebbleScreenshotImage, 0)
+		return nil
+	}
+
+	return json.Unmarshal(b, (*([]PebbleScreenshotImage))(psi))
+}
+
+func parseApp(path string, authors *map[string]int, lastAuthorId *int, categoriesNames, categoriesColors *map[string]string) *RebbleApplication {
 	f, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
@@ -78,14 +132,24 @@ func parseApp(path string, authors *map[string]int, lastId *int) *RebbleApplicat
 
 	// Create author if it doesn't exist
 	if _, ok := (*authors)[data.Apps[0].Author]; !ok {
-		(*authors)[data.Apps[0].Author] = *lastId + 1
-		*lastId = *lastId + 1
+		(*authors)[data.Apps[0].Author] = *lastAuthorId + 1
+		*lastAuthorId = *lastAuthorId + 1
+	}
+
+	// Create category if it doesn't exist
+	if _, ok := (*categoriesNames)[data.Apps[0].CategoryId]; !ok {
+		(*categoriesNames)[data.Apps[0].CategoryId] = data.Apps[0].CategoryName
+		(*categoriesColors)[data.Apps[0].CategoryId] = data.Apps[0].CategoryColor
 	}
 
 	app := RebbleApplication{}
+	app.AppInfo.Tags = make([]RebbleCategory, 1)
+	app.Assets.Screenshots = make([]string, 0)
 	app.Id = data.Apps[0].Id
 	app.Name = data.Apps[0].Name
-	app.Category = data.Apps[0].Category
+	app.AppInfo.Tags[0].Id = data.Apps[0].CategoryId
+	app.AppInfo.Tags[0].Name = data.Apps[0].CategoryName
+	app.AppInfo.Tags[0].Color = data.Apps[0].CategoryColor
 	app.Published = data.Apps[0].Published
 	app.Description = data.Apps[0].Description
 	app.Author = RebbleAuthor{(*authors)[data.Apps[0].Author], data.Apps[0].Author}
@@ -93,6 +157,18 @@ func parseApp(path string, authors *map[string]int, lastId *int) *RebbleApplicat
 	app.AppInfo.RebbleReady = false
 	app.AppInfo.Updated = data.Apps[0].Release.Published
 	app.AppInfo.Version = data.Apps[0].Release.Version
+	app.AppInfo.SupportUrl = ""
+	app.AppInfo.AuthorUrl = data.Apps[0].Website
+	app.AppInfo.SourceUrl = data.Apps[0].Source
+	if len(data.Apps[0].HeaderImages) > 0 {
+		app.Assets.Banner = data.Apps[0].HeaderImages[0].Orig
+	} else {
+		app.Assets.Banner = ""
+	}
+	app.Assets.Icon = ""
+	for _, screenshot := range data.Apps[0].Screenshots {
+		app.Assets.Screenshots = append(app.Assets.Screenshots, screenshot.Screenshot)
+	}
 
 	return &app
 }
@@ -168,19 +244,50 @@ func AppHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer db.Close()
-	rows, err := db.Query("SELECT apps.id, apps.name, apps.author_id, authors.name, apps.category, apps.description, apps.published_date, apps.pbw_url, apps.rebble_ready, apps.updated, apps.version FROM apps JOIN authors ON apps.author_id = authors.id WHERE apps.id=?", mux.Vars(r)["id"])
+	rows, err := db.Query("SELECT apps.id, apps.name, apps.author_id, authors.name, apps.tag_ids, apps.description, apps.published_date, apps.pbw_url, apps.rebble_ready, apps.updated, apps.version, apps.support_url, apps.author_url, apps.source_url, apps.screenshot_urls, apps.banner_url, apps.icon_url FROM apps JOIN authors ON apps.author_id = authors.id WHERE apps.id=?", mux.Vars(r)["id"])
 	if err != nil {
 		log.Fatal(err)
 	}
-	rows.Next()
+	exists := rows.Next()
+	if !exists {
+		w.WriteHeader(404)
+		w.Write([]byte("No application with this ID"))
+		return
+	}
+
 	app := RebbleApplication{}
 	var t_published, t_updated int64
-	err = rows.Scan(&app.Id, &app.Name, &app.Author.Id, &app.Author.Name, &app.Category, &app.Description, &t_published, &app.AppInfo.PbwUrl, &app.AppInfo.RebbleReady, &t_updated, &app.AppInfo.Version)
+	var tagIds_b []byte
+	var tagIds []string
+	var screenshots_b []byte
+	var screenshots []string
+	err = rows.Scan(&app.Id, &app.Name, &app.Author.Id, &app.Author.Name, &tagIds_b, &app.Description, &t_published, &app.AppInfo.PbwUrl, &app.AppInfo.RebbleReady, &t_updated, &app.AppInfo.Version, &app.AppInfo.SupportUrl, &app.AppInfo.AuthorUrl, &app.AppInfo.SourceUrl, &screenshots_b, &app.Assets.Banner, &app.Assets.Icon)
 	if err != nil {
 		log.Fatal(err)
 	}
 	app.Published.Time = time.Unix(0, t_published)
 	app.AppInfo.Updated.Time = time.Unix(0, t_updated)
+	json.Unmarshal(tagIds_b, &tagIds)
+	app.AppInfo.Tags = make([]RebbleCategory, len(tagIds))
+	json.Unmarshal(screenshots_b, &screenshots)
+
+	for i, tagId := range tagIds {
+		rows, err := db.Query("SELECT id, name, color FROM categories WHERE id=?", tagId)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rows.Next()
+		err = rows.Scan(&app.AppInfo.Tags[i].Id, &app.AppInfo.Tags[i].Name, &app.AppInfo.Tags[i].Color)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	app.Assets.Screenshots = make([]string, len(screenshots))
+	for i, screenshot := range screenshots {
+		app.Assets.Screenshots[i] = screenshot
+	}
 
 	data, err := json.MarshalIndent(app, "", "\t")
 	if err != nil {
